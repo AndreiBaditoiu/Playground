48eb597b6c9389508268186913905a41a42fbd75
Andrei <andrei.baditoiu@gmail.com>
2024-09-09 23:20:43 +0300

    Modified textme.py

--- textme/textme.py
+++ textme/textme.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
-account_sid = 'AC260b53286d7f7e12b7ab3f19663e76b4'
-auth_token = '4f44eeab64250b14949342449dbf60ca'
+account_sid = ''
+auth_token = '[AuthToken]'
 client = Client(account_sid, auth_token)
 message = client.messages.create(
-  messaging_service_sid='MGad478fcec26cc94799067d502d9160f6',
+  messaging_service_sid='',
   body='This is working just fine!!!',
-  to='+40725929880'
+  to=''
 )
 print(message.sid)

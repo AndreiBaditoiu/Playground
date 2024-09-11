170cb7ae9628fd95dcfeb7cb4f7139604ce09992
Andrei <andrei.baditoiu@gmail.com>
2024-09-11 22:16:20 +0300

    scraper. py final version

--- Screaper/scraper.py
+++ Screaper/scraper.py
@@ -1,12 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
 import pprint
 
+import requests
+from bs4 import BeautifulSoup
+
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+
+print(res, res2)
 
 links = soup.select('.titleline > a')
 subtext = soup.select('.subtext')
+links2 = soup2.select('.titleline > a')
+subtext2 = soup2.select('.subtext')
+
+super_links = links + links2
+super_subtext = subtext + subtext2
 
 
 def sort_stories_by_votes(hnlist):
@@ -19,7 +29,7 @@ def create_custom_hn(links, subtext):
         title = item.getText()
         href = item.get('href', None)
 
-        # Verificăm dacă există voturi pentru acest articol
+        # Check if there are votes for the article
         if idx < len(subtext):
             vote = subtext[idx].select('.score')
             if len(vote):
@@ -27,11 +37,11 @@ def create_custom_hn(links, subtext):
                 if points > 99:
                     hn.append({'title': title, 'link': href, 'votes': points})
         else:
-            # Tratează articolele fără voturi (de exemplu, poți decide să le ignori)
+            # Deal with articles without votes.
             print(f"Articolul '{title}' nu are voturi.")
 
     return sort_stories_by_votes(hn)
 
 
-# Afișează articolele cu minim 100 de voturi
-pprint.pprint(create_custom_hn(links, subtext))
+# Display articles that have at least 100 votes.
+pprint.pprint(create_custom_hn(super_links, super_subtext))
